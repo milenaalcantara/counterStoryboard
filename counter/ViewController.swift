ca6b91ce55ab19779da62a4202e3c37b0fa423e7
Milena <milimaalc.dev@gmail.com>
2025-03-31 11:40:16 -0300

    add counter

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBAction func incrementButton(_ sender: UIButton) {
+        countLabel.text = String(Int(countLabel.text ?? "0")! + 1)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
